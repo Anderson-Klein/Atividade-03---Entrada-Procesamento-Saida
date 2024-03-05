eec96880bfdad2ceb2ebc2b3676276e75f0823cb
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-03-04 22:30:47 -0300

    Create Atividade-3.java

---
+++ Atividade-3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        double num1 = 0.0;
+        double num2 = 0.0;;
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Insira o primeiro valor");
+    num1 = scanner.nextDouble();
+    
+    System.out.println("Insira o segundo valor");
+    num2 = scanner.nextDouble();
+    
+    double numt = (num1 + num2)/2;
+    
+    System.out.println("Valor total " + numt );
+    }
+}
