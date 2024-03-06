30878cb0d8cd09993de652d7dcb9fe56c52ecbe0
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-03-05 21:28:33 -0300

    Create Atividade-6.java

---
+++ Atividade-6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String nome = "";
+        double preco = 0.0;
+        double desconto = 0.0;
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Insira o nome do produto");
+    nome = scanner.nextLine();
+    
+    System.out.println("Insira o preco do produto");
+    preco = scanner.nextDouble();
+    
+    System.out.println("Insira o percentual de desconto");
+    desconto = scanner.nextDouble();
+    
+    double valorD = preco*(desconto/100);
+    double valorP = preco - valorD;
+    
+    System.out.println("nome:" + nome);
+    System.out.println("Valor a pagar" + valorP);
+
+    }
+}
